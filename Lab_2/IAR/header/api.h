9ce69c518e08855d68255910196be250177a36b4
mikileib <leibmi@post.bgu.ac.il>
2024-06-15 18:13:56 +0300

    to sabmit

---
+++ Lab_2/IAR/header/api.h
@@ -0,0 +1,18 @@
+#ifndef _api_H_
+#define _api_H_
+
+#include  "../header/halGPIO.h"     // private library - HAL layer
+
+
+extern void CountUp(void);
+extern void freqMeas(void);
+extern void tone_generator(void);
+
+#endif
+
+
+
+
+
+
+
